945a33bb23dde7f1ee056e60fafefeb5d99b5bbb
Christian Dupuis <cd@atomist.com>
2020-05-14 08:51:04 +0200

    Add chat message to inform about leaked secrets
    [changelog:added]

---
+++ lib/commands/addToWhitelist.ts
@@ -0,0 +1,75 @@
+import { CommandHandler } from "@atomist/skill/lib/handler";
+import { slackSuccessMessage } from "@atomist/skill/lib/messages";
+import { guid } from "@atomist/skill/lib/util";
+import * as _ from "lodash";
+import { ScanConfiguration } from "../scan";
+import {
+    SaveSkillConfigurationMutation,
+    SaveSkillConfigurationMutationVariables,
+} from "../typings/types";
+
+interface AddToWhitelistParameters {
+    config: string;
+    value: string;
+    msgId: string;
+}
+
+export const handler: CommandHandler<ScanConfiguration> = async ctx => {
+    const parameters = await ctx.parameters.prompt<AddToWhitelistParameters>({
+        config: {},
+        value: {},
+        msgId: { required: false },
+    });
+
+    const cfg = ctx.configuration.find(c => c.name === parameters.config);
+    if (!cfg) {
+        return {
+            code: 1,
+            reason: `Skill configuration not available`,
+        };
+    }
+    await ctx.graphql.mutate<SaveSkillConfigurationMutation, SaveSkillConfigurationMutationVariables>(
+        "saveSkillConfiguration.graphql",
+        {
+            name: ctx.skill.name,
+            namespace: ctx.skill.namespace,
+            version: ctx.skill.version,
+            config: {
+                enabled: true,
+                name: cfg.name,
+                parameters: [{
+                    stringArray: {
+                        name: "glob",
+                        value: cfg.parameters.glob,
+                    },
+                }, {
+                    stringArray: {
+                        name: "pattern",
+                        value: cfg.parameters.pattern,
+                    },
+                }, {
+                    multiChoice: {
+                        name: "disabled",
+                        value: cfg.parameters.disabled,
+                    },
+                }, {
+                    stringArray: {
+                        name: "whitelist",
+                        value: [...(cfg.parameters.whitelist || []), parameters.value],
+                    },
+                }],
+                resourceProviders: _.map(cfg.resourceProviders, (v, k) => ({ name: k, selectedResourceProviders: v.selectedResourceProviders })),
+            },
+        });
+
+    await ctx.message.respond(slackSuccessMessage(
+        "Secret Scanner",
+        `Successfully added \`${parameters.value}\` to whitelist for configuration _${cfg.name}_`,
+        ctx),
+        { id: parameters.msgId || guid() });
+
+    return {
+        code: 0,
+        reason: `Updated whitelist for configuration \`${cfg.name}\``,
+    };
+};
